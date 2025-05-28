29a9191fd8aae0ae86b5631ce5cab8cc17a601ae
andrew <andrewcs149@gmail.com>
2025-05-28 11:55:45 -0400

    update

--- Entities/Units/Unit.cs
+++ Entities/Units/Unit.cs
@@ -2,7 +2,6 @@ using Godot;
 
 public partial class Unit : Node3D
 {
-	private MeshInstance3D _meshInstance3D;
 	private Sprite3D _selectBorder;
 	private bool _selected = false;
 	public bool Selected
@@ -20,7 +19,6 @@ public partial class Unit : Node3D
 
 	public override void _Ready()
 	{
-		_meshInstance3D = GetNode<MeshInstance3D>("MeshInstance3D");
 		_selectBorder = GetNode<Sprite3D>("SelectBorder");
 		_selectBorder.Visible = false;
 	}
