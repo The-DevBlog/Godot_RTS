984f9c09a9f1ef565f5b88dae2a67ce87549566d
andrew <andrewcs149@gmail.com>
2025-06-03 14:57:58 -0400

    update

--- Scripts/UI/StructureBtn.cs
+++ Scripts/UI/StructureBtn.cs
@@ -3,19 +3,19 @@ using MyEnums;
 
 public partial class StructureBtn : Button
 {
-    [Export]
-    public Structure Structure { get; set; }
+	[Export]
+	public Structure Structure { get; set; }
 
-    public override void _Ready()
-    {
-        if (Structure == Structure.None)
-            Utils.PrintErr("Structure Enum is not set for " + Name);
+	public override void _Ready()
+	{
+		if (Structure == Structure.None)
+			Utils.PrintErr("Structure Enum is not set for " + Name);
 
-        Pressed += OnButtonPressed;
-    }
+		Pressed += OnButtonPressed;
+	}
 
-    private void OnButtonPressed()
-    {
-        GD.Print("Button pressed for structure: " + Structure);
-    }
+	private void OnButtonPressed()
+	{
+		GD.Print("Button pressed for structure: " + Structure);
+	}
 }
