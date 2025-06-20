6c1f730e1e3d5d4224f5821d4d8c3b38a94d4f66
andrew <andrewcs149@gmail.com>
2025-06-20 08:09:02 -0400

    update

--- Scripts/Shared/SceneResources.cs
+++ Scripts/Shared/SceneResources.cs
@@ -2,18 +2,18 @@ using Godot;
 
 public partial class SceneResources : Node3D
 {
-    public static SceneResources Instance { get; set; }
-    [Export] public Vector2 MapSize { get; set; }
-    [Export] public int Funds { get; set; }
-    public int Energy { get; set; }
-    public int EnergyConsumed { get; set; }
+	public static SceneResources Instance { get; set; }
+	[Export] public Vector2 MapSize { get; set; }
+	[Export] public int Funds { get; set; }
+	public int Energy { get; set; }
+	public int EnergyConsumed { get; set; }
 
-    public override void _EnterTree()
-    {
-        base._EnterTree();  // **must** call this first
-        Instance = this;
+	public override void _EnterTree()
+	{
+		base._EnterTree();  // **must** call this first
+		Instance = this;
 
-        if (Funds == 0)
-            Utils.PrintErr("No Funds Assigned");
-    }
+		if (Funds == 0)
+			Utils.PrintErr("No Funds Assigned");
+	}
 }
