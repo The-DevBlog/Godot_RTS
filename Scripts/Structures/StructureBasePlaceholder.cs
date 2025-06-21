a384b43a36d7bc1661b89e4aafebbb072bac00d8
andrew <andrewcs149@gmail.com>
2025-06-21 13:10:21 -0400

    update

--- Scripts/Structures/StructureBasePlaceholder.cs
+++ Scripts/Structures/StructureBasePlaceholder.cs
@@ -6,7 +6,7 @@ public partial class StructureBasePlaceholder : StructureBase
 	[Export] MeshInstance3D ValidityRing { get; set; }
 	public readonly HashSet<Area3D> Overlaps = new();
 	public bool ValidPlacement => Overlaps.Count == 0;
-
+	private bool _lastValidState = true;
 	private ShaderMaterial _validityShader;
 
 	public override void _Ready()
@@ -17,6 +17,7 @@ public partial class StructureBasePlaceholder : StructureBase
 
 		Area.AreaEntered += OnAreaEntered;
 		Area.AreaExited += OnAreaExited;
+
 		_validityShader = ValidityRing.GetSurfaceOverrideMaterial(0) as ShaderMaterial;
 	}
 
@@ -26,12 +27,28 @@ public partial class StructureBasePlaceholder : StructureBase
 			return;   // ignore self-entering
 
 		Overlaps.Add(other);
+
+		UpdateValidityShader();
 		// optional: update visuals here
 	}
 
 	public void OnAreaExited(Area3D other)
 	{
 		Overlaps.Remove(other);
+
+		UpdateValidityShader();
 		// optional: update visuals here
+	}
+
+	private void UpdateValidityShader()
+	{
+		bool nowValid = ValidPlacement;
+		// only flip if it really changed
+		if (nowValid == _lastValidState)
+			return;
+		_lastValidState = nowValid;
+
+		// this assumes your shader has a `uniform bool valid_placement;`
+		_validityShader.SetShaderParameter("valid_placement", nowValid);
 	}
 }
