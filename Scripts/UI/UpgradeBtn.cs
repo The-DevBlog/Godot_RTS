e8026c8dbc39bc453affc65b5193fea8fd439f1e
andrew <andrewcs149@gmail.com>
2025-06-24 18:35:44 -0400

    update

--- Scripts/UI/UpgradeBtn.cs
+++ Scripts/UI/UpgradeBtn.cs
@@ -3,23 +3,23 @@ using MyEnums;
 
 public partial class UpgradeBtn : Button
 {
-	[Export] public UpgradeType Upgrade { get; set; }
-	private SceneResources _sceneResources;
-	private Signals _signals;
+    [Export] public UpgradeType Upgrade { get; set; }
+    private SceneResources _sceneResources;
+    private Signals _signals;
 
-	public override void _Ready()
-	{
-		if (Upgrade == UpgradeType.None)
-			Utils.PrintErr("Upgrade type is set to none");
+    public override void _Ready()
+    {
+        if (Upgrade == UpgradeType.None)
+            Utils.PrintErr("Upgrade type is set to none");
 
-		_sceneResources = SceneResources.Instance;
+        _sceneResources = SceneResources.Instance;
 
-		_signals = Signals.Instance;
-		_signals.UpdateUpgradesAvailability += EnableDisableBtns;
-	}
+        _signals = Signals.Instance;
+        _signals.UpdateUpgradesAvailability += EnableDisableBtns;
+    }
 
-	private void EnableDisableBtns()
-	{
-		Disabled = !_sceneResources.UpgradeAvailability[Upgrade];
-	}
+    private void EnableDisableBtns()
+    {
+        Disabled = !_sceneResources.UpgradeAvailability[Upgrade];
+    }
 }
