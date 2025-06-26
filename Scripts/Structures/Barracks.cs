11dd1ec066991c5c59dc24c1e466b73754efc515
Andrew Smith <andrewcs149@gmail.com>
2025-06-26 07:56:39 -0400

    uppdate

--- Scripts/Structures/Barracks.cs
+++ Scripts/Structures/Barracks.cs
@@ -24,6 +24,28 @@ public partial class Barracks : StructureBase
 
 	private void BuildInfantry(Infantry infantry)
 	{
+		var sceneRoot = GetTree().CurrentScene;
+		sceneRoot.AddChild(infantry);
+
+		// how far in front of the garage you want to spawn:
+		float spawnDistance = 6f;
+
+		// take your garage’s global transform…
+		var gtf = this.GlobalTransform;
+		// compute forward as –Z:
+		Vector3 forward = gtf.Basis.Z.Normalized();
+
+		// build a new transform for the vehicle’s origin:
+		var spawnT = gtf;
+		spawnT.Origin += forward * spawnDistance;
+
+		// now *rotate* the basis 180° around the Y axis so it points backwards:
+		//   Mathf.Pi radians is 180 degrees
+		var flippedBasis = spawnT.Basis.Rotated(Vector3.Up, Mathf.Pi);
+		spawnT.Basis = flippedBasis;
+
+		infantry.GlobalTransform = spawnT;
+
 		GD.Print($"Building {infantry.Name} in Barracks {Id}");
 	}
 }
