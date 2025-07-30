f800c8cda067b71a448c94fafd6b780dd655aef1
Andrew Smith <andrewcs149@gmail.com>
2025-07-29 21:51:02 -0400

    update

--- Scripts/Menus/LobbyMenu/PlayerContainer.cs
+++ Scripts/Menus/LobbyMenu/PlayerContainer.cs
@@ -1,76 +1,3 @@
-// using Godot;
-// using System.Collections.Generic;
-
-// public partial class PlayerContainer : PanelContainer
-// {
-// 	[Export] public int PlayerId;
-// 	[Export] public Label PlayerIdLabel;
-// 	[Export] public Label _fundsLabel;
-// 	[Export] private OptionButton _colorOptionButton;
-// 	[Export] private OptionButton _teamOptionButton;
-// 	[Export] private Color _selectedColor;
-// 	private Dictionary<int, Color> _colors;
-
-// 	public override void _Ready()
-// 	{
-// 		_colors = new()
-// 		{
-// 			{ 0, new Color("#d13f4b") }, // Red
-// 			{ 1, new Color("#678746") }, // Green
-// 			{ 2, new Color("#5090b3") }, // Blue
-// 			{ 3, new Color("#a26f2a") }, // Orange
-// 		};
-
-// 		Utils.NullExportCheck(PlayerIdLabel);
-// 		Utils.NullExportCheck(_colorOptionButton);
-// 		Utils.NullExportCheck(_fundsLabel);
-// 		Utils.NullExportCheck(_teamOptionButton);
-
-// 		RemoveOptionCheckbox(_colorOptionButton);
-// 		RemoveOptionCheckbox(_teamOptionButton);
-
-// 		_colorOptionButton.ItemSelected += idx =>
-// 		{
-// 			// then locally apply it too:
-// 			ChangeColor((int)idx);
-// 		};
-
-// 		// Disable changeable properties if not server
-// 		if (!Multiplayer.IsServer())
-// 		{
-// 			_colorOptionButton.Disabled = true;
-// 			_teamOptionButton.Disabled = true;
-// 		}
-
-// 		PlayerIdLabel.Text = $"Player {PlayerId}";
-// 	}
-
-// 	private void ChangeColor(int idx)
-// 	{
-// 		var shared = _colorOptionButton.GetThemeStylebox("normal") as StyleBoxFlat;
-// 		var style = shared?.Duplicate() as StyleBoxFlat ?? new StyleBoxFlat();
-
-// 		style.BgColor = _colors[idx];
-// 		_colorOptionButton.AddThemeStyleboxOverride("normal", style);
-// 		_colorOptionButton.AddThemeStyleboxOverride("hover", style);
-// 		_colorOptionButton.AddThemeStyleboxOverride("focus", style);
-// 	}
-
-// 	private void RemoveOptionCheckbox(OptionButton optionButton)
-// 	{
-// 		// Get the popup menu behind this OptionButton
-// 		var popup = optionButton.GetPopup();
-
-// 		// Loop over every item and disable its radio check
-// 		int count = popup.GetItemCount();
-// 		for (int i = 0; i < count; i++)
-// 		{
-// 			if (popup.IsItemRadioCheckable(i))
-// 				popup.SetItemAsRadioCheckable(i, false);
-// 		}
-// 	}
-// }
-
 using Godot;
 using System.Collections.Generic;
 
@@ -78,22 +5,23 @@ public partial class PlayerContainer : PanelContainer
 {
 	[Export] public int PlayerId { get; set; }
 	[Export] public Label PlayerIdLabel;
-	[Export] public Label _fundsLabel;
+	[Export] private Label _fundsLabel;
+	[Export] private CheckButton _readyupBtn;
 	[Export] private OptionButton _colorOptionButton;
 	[Export] private OptionButton _teamOptionButton;
 	[Export]
-	public Color ReplicatedColor
+	public Color PlayerColor
 	{
-		get => _replicatedColor;
+		get => _playerColor;
 		set
 		{
-			_replicatedColor = value;
-			ApplyStyleboxColor(_replicatedColor);
+			_playerColor = value;
+			ApplyColor(_playerColor);
 		}
 	}
 
 	// Replicated color property
-	private Color _replicatedColor = new Color("#d13f4b");
+	private Color _playerColor = new Color("#d13f4b");
 
 	private Dictionary<int, Color> _colors;
 
@@ -112,12 +40,13 @@ public partial class PlayerContainer : PanelContainer
 		Utils.NullExportCheck(_colorOptionButton);
 		Utils.NullExportCheck(_fundsLabel);
 		Utils.NullExportCheck(_teamOptionButton);
+		Utils.NullExportCheck(_readyupBtn);
 
 		RemoveOptionCheckbox(_colorOptionButton);
 		RemoveOptionCheckbox(_teamOptionButton);
 
 		// Apply the replicated color on spawn or change
-		ApplyStyleboxColor(_replicatedColor);
+		ApplyColor(_playerColor);
 
 		// Only the server may change color (clients see disabled control)
 		_colorOptionButton.ItemSelected += idx =>
@@ -125,7 +54,7 @@ public partial class PlayerContainer : PanelContainer
 			if (!Multiplayer.IsServer())
 				return;
 			// Set the replicated property (Sync on Change)
-			ReplicatedColor = _colors[(int)idx];
+			PlayerColor = _colors[(int)idx];
 		};
 
 		// Disable interactive controls on clients
@@ -139,7 +68,7 @@ public partial class PlayerContainer : PanelContainer
 		PlayerIdLabel.Text = $"Player {PlayerId}";
 	}
 
-	private void ApplyStyleboxColor(Color color)
+	private void ApplyColor(Color color)
 	{
 		var shared = _colorOptionButton.GetThemeStylebox("normal") as StyleBoxFlat;
 		var style = shared?.Duplicate() as StyleBoxFlat ?? new StyleBoxFlat();
@@ -147,6 +76,8 @@ public partial class PlayerContainer : PanelContainer
 		_colorOptionButton.AddThemeStyleboxOverride("normal", style);
 		_colorOptionButton.AddThemeStyleboxOverride("hover", style);
 		_colorOptionButton.AddThemeStyleboxOverride("focus", style);
+
+		// ResetReadyUp();
 	}
 
 	private void RemoveOptionCheckbox(OptionButton optionButton)
@@ -158,5 +89,11 @@ public partial class PlayerContainer : PanelContainer
 			if (popup.IsItemRadioCheckable(i))
 				popup.SetItemAsRadioCheckable(i, false);
 		}
+	}
+
+	// Resets the ready-up state for a player if any of his settings are changed
+	private void ResetReadyUp(int idx)
+	{
+		_readyupBtn.SetPressedNoSignal(false);
 	}
 }
